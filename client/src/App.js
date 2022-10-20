4a4a593437d6501e41c2755ea7a5cf1a09605443
seantokuzo <seantokuzo@gmail.com>
2022-10-20 13:52:05 -0700

    import new components and render

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React from "react"
-import CreatePost from "./CreatePost"
+import React from 'react'
+import CreatePost from './CreatePost'
+import PostList from './PostList'
 
 const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
       <CreatePost />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   )
 }
