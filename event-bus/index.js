f9992f7a9c0726afee2806245bd2720ab21ec2f3
seantokuzo <seantokuzo@gmail.com>
2022-10-27 14:13:11 -0700

    update event bus for moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,35 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req, res) => {
   const event = req.body
 
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
-  axios.post('http://localhost:4002/events', event)
+  events.push(event)
+
+  // POSTS SERVICE
+  axios
+    .post('http://localhost:4000/events', event)
+    .catch((err) => console.log(err.message))
+  // COMMENTS SERVICE
+  axios
+    .post('http://localhost:4001/events', event)
+    .catch((err) => console.log(err.message))
+  // QUERY SERVICE
+  axios
+    .post('http://localhost:4002/events', event)
+    .catch((err) => console.log(err.message))
+  // MODERATION SERVICE
+  axios
+    .post('http://localhost:4003/events', event)
+    .catch((err) => console.log(err.message))
 
   res.send({ status: 'OK' })
+})
+
+app.get('/events', (req, res) => {
+  res.send(events)
 })
 
 app.listen(4005, () => {
